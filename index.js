662b5a3a08e65fb98e6c8f154fa91c6a27fd01bd
tchami <131756888+tchamianest@users.noreply.github.com>
2024-02-12 15:52:46 +0200

    changes

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const namea = "hello word";
-console.log(namea);
+const fs = require("fs");
+const http = require("http");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+const server = http.createServer((req, res) => {
+  console.log("welcome to the fiirst serve");
+  res.end("welcome");
+});
+
+server.listen(3000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 3000");
+});
