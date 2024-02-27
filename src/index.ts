903fb44a9cbbe2563571f0bad71bf36fc445d3d6
tchami <131756888+tchamianest@users.noreply.github.com>
2024-02-27 17:03:06 +0200

    swagger and refactory
    1.make all swagger test documentation
    2.add cycle CI

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,21 @@ import mongoose from "mongoose";
 
 import "./jwt/authe";
 import app from "./app";
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "./swagger.json";
+import { resolve } from "path";
+
 ///BODY PARSER
 const DB: any = process.env.MONGO_DB_CONNECT;
 ////CONNECTING TO MY DATABASE
-const PORT = 8000;
+const PORT: any = 8000;
 mongoose
   .connect(DB)
   .then(() => {
     app.listen(PORT, () => {
       console.log("welcome");
+
+      app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     });
   })
 
